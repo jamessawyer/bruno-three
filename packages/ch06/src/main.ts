192e2f3a7f88f794d22e4bfdb1d8cb66b87dac28
jinx <jinx@example.com>
2026-02-14 17:37:32 +0800

    ch06(相机): 添加OrbitControls控制相机移动，介绍正交相机和透视相机

--- packages/ch06/src/main.ts
+++ packages/ch06/src/main.ts
@@ -1,5 +1,15 @@
 import * as T from "three";
-import gsap from "gsap";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { Pane } from "tweakpane";
+
+const cursor = {
+  x: 0,
+  y: 0,
+};
+window.addEventListener("mousemove", (evt: MouseEvent) => {
+  cursor.x = evt.clientX / sizes.width - 0.5;
+  cursor.y = -(evt.clientY / sizes.height - 0.5);
+});
 
 // Canvas
 const canvas = document.getElementById("app") as HTMLCanvasElement;
@@ -9,40 +19,10 @@ const scene = new T.Scene();
 
 // Objects
 const group = new T.Group();
-group.position.y = 1;
-group.scale.y = 1;
-// group.rotation.y = Math.PI / 4;
 scene.add(group);
 
 const cube1 = new T.Mesh(new T.BoxGeometry(1, 1, 1), new T.MeshBasicMaterial({ color: 0xff0000 }));
 group.add(cube1);
-
-// const cube2 = new T.Mesh(new T.BoxGeometry(1, 1, 1), new T.MeshBasicMaterial({ color: 0x00ff00 }));
-// cube2.position.x = -2;
-// group.add(cube2);
-
-// const cube3 = new T.Mesh(new T.BoxGeometry(1, 1, 1), new T.MeshBasicMaterial({ color: 0x0000ff }));
-// cube3.position.x = 2;
-// group.add(cube3);
-
-// // Object
-// const geometry = new T.BoxGeometry(1, 1, 1);
-// const material = new T.MeshBasicMaterial({ color: 0xff0000 });
-// const mesh = new T.Mesh(geometry, material);
-// scene.add(mesh);
-
-// // Position
-// mesh.position.set(0.7, -0.6, 1);
-
-// // Scale
-// mesh.scale.x = 2;
-// mesh.scale.y = 0.5;
-// mesh.scale.z = 0.5;
-
-// // Rotation
-// mesh.rotation.reorder("YXZ"); // 改变旋转之前先进行reorder才会按照指定顺序旋转
-// mesh.rotation.x = Math.PI / 4;
-// mesh.rotation.y = Math.PI / 4;
 
 // Axis Helper
 const axisHelper = new T.AxesHelper(2);
@@ -55,16 +35,22 @@ const sizes = {
 };
 
 // Camera
-const camera = new T.PerspectiveCamera(75, sizes.width / sizes.height);
+
+// 透视相机
+const camera = new T.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+
+// 正交相机
+// 为了保证正交相机的纵横比与透视相机一致
+// const aspectRatio = sizes.width / sizes.height;
+// const camera = new T.OrthographicCamera(-aspectRatio, aspectRatio, 1, -1, 0.1, 100);
+
 camera.position.z = 3;
-// camera.position.x = 1;
-// camera.position.y = 1;
+camera.lookAt(group.position);
 scene.add(camera);
 
-// 相机看向哪里。默认看向坐标原点
-// camera.lookAt(mesh.position);
-
-// console.log(mesh.position.distanceTo(camera.position));
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true; // 必须在 tick() 中调用 controls.update()
 
 // Renderer
 const renderer = new T.WebGLRenderer({
@@ -73,61 +59,27 @@ const renderer = new T.WebGLRenderer({
 
 renderer.setSize(sizes.width, sizes.height);
 
-/** 方式1 手动计算时间差 */
-// let time = Date.now();
+// Tweakpane
+const pane = new Pane();
+// 正交相机
+// pane.addBinding(camera, "zoom", { min: 0.2, max: 5, step: 0.1 }).on("change", () => {
+//   camera.updateProjectionMatrix();
+// });
 
-// // Animations
-// const tick = () => {
-//   // 通过这种方式，可以确保动画在不同帧率下都能保持一致的速度
-//   const current = Date.now();
-//   const delta = current - time;
-//   time = current;
-
-//   // Update objects
-//   group.rotation.y += delta * 0.001;
-
-//   renderer.render(scene, camera);
-
-//   requestAnimationFrame(tick);
-// };
-
-/** 方式2 使用Clock */
-// const clock = new T.Clock();
-
-// const tick = () => {
-//   const elapsedTime = clock.getElapsedTime();
-//   // console.log(elapsedTime);
-
-//   // Update objects
-//   // 方式1 - 运动物体
-//   // group.rotation.y = elapsedTime * Math.PI * 2;  // 每秒旋转1圈
-
-//   // 运动物体 - 圆圈运动
-//   // group.position.y = Math.sin(elapsedTime); // 上下运动
-//   // group.position.x = Math.cos(elapsedTime); // 左右运动
-
-//   // 运动相机
-//   camera.position.x = Math.cos(elapsedTime) * 3;
-//   camera.position.y = Math.sin(elapsedTime) * 3;
-//   camera.lookAt(group.position); // 相机看向group的中心位置
-
-//   renderer.render(scene, camera);
-
-//   requestAnimationFrame(tick);
-// };
-// tick();
-
-/** 方式3：gsap */
-gsap.to(group.position, {
-  duration: 1,
-  x: 2,
-  repeatDelay: 0.1,
-  yoyo: true,
-  repeat: -1,
+// 透视相机
+pane.addBinding(camera, "fov", { min: 1, max: 150, step: 0.1 }).on("change", () => {
+  camera.updateProjectionMatrix();
 });
 
 const tick = () => {
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  // camera.position.y = cursor.y * 5;
+  // camera.lookAt(group.position);
+
+  controls.update();
   renderer.render(scene, camera);
+
   requestAnimationFrame(tick);
 };
 
