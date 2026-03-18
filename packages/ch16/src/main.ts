0eea7f2ecd70914be35cdd11c1bc3671b61b6133
jinx <jinx@example.com>
2026-03-18 21:18:26 +0800

    ch16(Haunted House): 添加天空对象以增强场景氛围
    1. 引入Sky对象并设置其属性，以创建动态天空效果。
    2. 调整天空的材质参数，提升场景的视觉表现。

--- packages/ch16/src/main.ts
+++ packages/ch16/src/main.ts
@@ -1,5 +1,6 @@
 import * as T from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
+import { Sky } from "three/addons/objects/Sky.js";
 import { Pane } from "tweakpane";
 
 // --- Tweakpane 调试面板 ---
@@ -445,6 +446,18 @@ ghost3.shadow.mapSize.set(256, 256);
 ghost1.shadow.camera.far = 10;
 ghost2.shadow.camera.far = 10;
 ghost3.shadow.camera.far = 10;
+
+/**
+ * Sky
+ */
+const sky = new Sky();
+sky.scale.setScalar(100);
+scene.add(sky);
+sky.material.uniforms["turbidity"]!.value = 10;
+sky.material.uniforms["rayleigh"]!.value = 3;
+sky.material.uniforms["mieCoefficient"]!.value = 0.1;
+sky.material.uniforms["mieDirectionalG"]!.value = 0.95;
+sky.material.uniforms["sunPosition"]!.value.set(0.3, -0.038, -0.95);
 
 // --- 适配窗口 ---
 window.addEventListener("resize", () => {
