29bbae2833d56c21d18da6de1feb436f6d8b21db
jinx <jinx@example.com>
2026-03-22 16:35:27 +0800

    ch17(Particles): 调整设置

--- packages/ch17/src/main.ts
+++ packages/ch17/src/main.ts
@@ -1,11 +1,25 @@
 import * as T from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
+// Canvas
+const canvas = document.getElementById("app") as HTMLCanvasElement;
+
 // --- 初始化场景 ---
 const scene = new T.Scene();
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+// Camera
 const camera = new T.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 3;
+
+// Renderer
 const renderer = new T.WebGLRenderer({
-  canvas: document.getElementById("app") as HTMLCanvasElement,
+  canvas,
   antialias: true,
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -16,10 +30,13 @@ const geometry = new T.IcosahedronGeometry(1, 4);
 const material = new T.MeshNormalMaterial({ wireframe: true });
 const mesh = new T.Mesh(geometry, material);
 scene.add(mesh);
-camera.position.z = 3;
 
+// Controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
+
+const axisHelper = new T.AxesHelper(2);
+scene.add(axisHelper);
 
 // --- 动画循环 ---
 function animate() {
@@ -30,8 +47,15 @@ function animate() {
 animate();
 
 // --- 适配窗口 ---
+// resize 事件
 window.addEventListener("resize", () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // 更新相机
+  camera.aspect = sizes.width / sizes.height; // 更新宽高比
+  camera.updateProjectionMatrix(); // 更新相机投影矩阵
+
+  renderer.setSize(sizes.width, sizes.height); // 更新渲染器大小
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
