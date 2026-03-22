a4a5fc5bda9ebedfe11ab16e1a2a50cf6b0e7c7a
jinx <jinx@example.com>
2026-03-22 20:25:22 +0800

    build: 更新create-chapter模板

--- scripts/create-chapter.ts
+++ scripts/create-chapter.ts
@@ -83,13 +83,25 @@ await write(
   join(targetDir, "src/main.ts"),
   `
 import * as T from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { Pane } from 'tweakpane';
+
+const canvas = document.getElementById('app') as HTMLCanvasElement
 
 // --- 初始化场景 ---
 const scene = new T.Scene();
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
 const camera = new T.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// Renderer
 const renderer = new T.WebGLRenderer({ 
-  canvas: document.getElementById('app') as HTMLCanvasElement, 
+  canvas, 
   antialias: true 
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -115,19 +127,37 @@ pane.addBinding(params, 'wireframe', { label: '线框模式' }).on('change', (ev
   material.wireframe = ev.value;
 });
 
+// Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
 // --- 动画循环 ---
-function animate() {
-  requestAnimationFrame(animate);
+const timer = new T.Timer();
+
+function tick() {
+  timer.update();
+  
+  controls.update();
+
+  const elapsedTime = timer.getElapsed();
   mesh.rotation.y += params.rotationSpeed;
+
   renderer.render(scene, camera);
+  requestAnimationFrame(tick);
 }
-animate();
+tick();
 
 // --- 适配窗口 ---
-window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // 更新相机
+  camera.aspect = sizes.width / sizes.height; // 更新宽高比
+  camera.updateProjectionMatrix(); // 更新相机投影矩阵
+
+  renderer.setSize(sizes.width, sizes.height); // 更新渲染器大小
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 `,
 );
