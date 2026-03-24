1b73210d2776c39ddb67ecacee44803392380381
jinx <jinx@example.com>
2026-03-24 22:13:07 +0800

    ch18(Galaxy): 实现星系生成逻辑并添加调试面板
    1. 添加了星系生成函数，支持自定义参数如数量、大小、半径、分支、旋转和颜色。
    2. 引入Tweakpane调试面板，允许实时调整星系参数并更新显示。
    3. 更新相机位置并添加相机参数调节功能，增强用户交互体验。

--- packages/ch18/src/main.ts
+++ packages/ch18/src/main.ts
@@ -10,7 +10,132 @@ const scene = new T.Scene();
 /**
  * Galaxy
  */
-const generateGalaxy = () => {};
+const parameters: Record<string, any> = {};
+parameters.count = 100_000;
+parameters.size = 0.001;
+parameters.radius = 5;
+parameters.branches = 8; // 悬臂数量
+parameters.spin = 1.0; // 悬臂旋转角度
+parameters.randomness = 0.2; // 悬臂随机性(暂时没用，使用下面的randomnessPower来实现)
+parameters.randomnessPower = 4; // 悬臂随机性强度
+parameters.insideColor = "#ff6030";
+parameters.outsideColor = "#1b3984";
+
+let geometry: T.BufferGeometry;
+let material: T.PointsMaterial;
+let points: T.Points;
+
+const generateGalaxy = () => {
+  // 消除旧的Galaxy
+  if (points !== undefined) {
+    geometry.dispose();
+    material.dispose();
+    scene.remove(points);
+  }
+
+  geometry = new T.BufferGeometry();
+
+  const positions = new Float32Array(parameters.count * 3);
+  const colors = new Float32Array(parameters.count * 3);
+  const colorInside = new T.Color(parameters.insideColor);
+  const colorOutside = new T.Color(parameters.outsideColor);
+
+  for (let i = 0; i < parameters.count; i++) {
+    const i3 = i * 3;
+    const radius = Math.random() * parameters.radius;
+    const spinAngle = radius * parameters.spin;
+    const branchAngle = ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
+
+    // 💡 通过 Math.pow() 来实现随机性的分布曲线
+    const randomX =
+      Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1);
+    const randomY =
+      Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1);
+    const randomZ =
+      Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1);
+
+    positions[i3] = Math.cos(branchAngle + spinAngle) * radius + randomX;
+    positions[i3 + 1] = randomY;
+    positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ;
+
+    // 颜色
+    // 这里使用 clone() 避免修改colorInside的值
+    const mixedColor = colorInside.clone().lerp(colorOutside, radius / parameters.radius);
+    colors[i3] = mixedColor.r;
+    colors[i3 + 1] = mixedColor.g;
+    colors[i3 + 2] = mixedColor.b;
+  }
+
+  geometry.setAttribute("position", new T.BufferAttribute(positions, 3));
+  geometry.setAttribute("color", new T.BufferAttribute(colors, 3));
+
+  /**
+   * Material
+   */
+  material = new T.PointsMaterial({
+    size: parameters.size,
+    sizeAttenuation: true, // 根据相机距离缩放粒子大小
+    depthWrite: false, // 停用深度写入，能够会遮挡其它物体
+    blending: T.AdditiveBlending, // 添加混合，让粒子颜色叠加，而不是覆盖
+    vertexColors: true,
+  });
+
+  points = new T.Points(geometry, material);
+  scene.add(points);
+};
+
+generateGalaxy();
+
+// --- Tweakpane 调试面板 ---
+const pane = new Pane({ title: "参数调试", expanded: true });
+pane.addBinding(parameters, "count", { min: 100, max: 1_000_000, step: 100 }).on("change", (ev) => {
+  if (ev.last) {
+    // 相当于onFinishChange
+    generateGalaxy();
+  }
+});
+pane.addBinding(parameters, "size", { min: 0.001, max: 0.1, step: 0.001 }).on("change", (ev) => {
+  if (ev.last) {
+    generateGalaxy();
+  }
+});
+pane.addBinding(parameters, "radius", { min: 1, max: 20, step: 0.01 }).on("change", (ev) => {
+  if (ev.last) {
+    generateGalaxy();
+  }
+});
+pane.addBinding(parameters, "branches", { min: 2, max: 20, step: 1 }).on("change", (ev) => {
+  if (ev.last) {
+    generateGalaxy();
+  }
+});
+pane.addBinding(parameters, "spin", { min: -5, max: 5, step: 0.01 }).on("change", (ev) => {
+  if (ev.last) {
+    generateGalaxy();
+  }
+});
+pane.addBinding(parameters, "randomness", { min: 0, max: 1, step: 0.01 }).on("change", (ev) => {
+  if (ev.last) {
+    generateGalaxy();
+  }
+});
+pane
+  .addBinding(parameters, "randomnessPower", { min: 1, max: 10, step: 0.01 })
+  .on("change", (ev) => {
+    if (ev.last) {
+      generateGalaxy();
+    }
+  });
+pane.addBinding(parameters, "insideColor", { picker: "inline" }).on("change", (ev) => {
+  if (ev.last) {
+    generateGalaxy();
+  }
+});
+pane.addBinding(parameters, "outsideColor", { picker: "inline" }).on("change", (ev) => {
+  if (ev.last) {
+    generateGalaxy();
+  }
+});
 
 // Sizes
 const sizes = {
@@ -19,6 +144,30 @@ const sizes = {
 };
 
 const camera = new T.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0.43, 5.0, 10);
+
+const cameraFolder = pane.addFolder({ title: "Camera", expanded: true });
+cameraFolder
+  .addBinding(camera.position, "x", { min: -10, max: 10, step: 0.01 })
+  .on("change", (ev) => {
+    if (ev.last) {
+      generateGalaxy();
+    }
+  });
+cameraFolder
+  .addBinding(camera.position, "y", { min: -10, max: 10, step: 0.01 })
+  .on("change", (ev) => {
+    if (ev.last) {
+      generateGalaxy();
+    }
+  });
+cameraFolder
+  .addBinding(camera.position, "z", { min: -10, max: 10, step: 0.01 })
+  .on("change", (ev) => {
+    if (ev.last) {
+      generateGalaxy();
+    }
+  });
 
 // Renderer
 const renderer = new T.WebGLRenderer({
@@ -27,11 +176,6 @@ const renderer = new T.WebGLRenderer({
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-camera.position.z = 3;
-
-// --- Tweakpane 调试面板 ---
-const pane = new Pane({ title: "参数调试" });
 
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
